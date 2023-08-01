4146848e9fb680d46bd1b7a216511d9ee2062fab
Sergio <sergiadn335@gmail.com>
2023-08-01 15:09:56 +0200

    cleaning index

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
-const app = require('express')()
 const express = require('express')
+const app = express()
 require('dotenv').config()
-const { v4 } = require('uuid')
+// const { v4 } = require('uuid')
 const path = require('path')
-const pug = require('pug')
+// const pug = require('pug')
 const publicDir = express.static(path.join('api', 'public'))
 const dbConnect = require('./config/mongo')
 const viewDir = path.join(__dirname, 'views')
@@ -11,7 +11,7 @@ const port = (process.env.port || 3000)
 const cookieParser = require('cookie-parser')
 const routes = require('./routes/index')
 app
-// Configurando app esto no va
+// Configurando app
   .set('views', viewDir)
   .set('view engine', 'pug')
 app
@@ -20,17 +20,17 @@ app
   .use(cookieParser())
   .use('/', routes)
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`
-  res.setHeader('Content-Type', 'text/html')
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`)
-})
+// app.get('/api', (req, res) => {
+//   const path = `/api/item/${v4()}`
+//   res.setHeader('Content-Type', 'text/html')
+//   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+//   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`)
+// })
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params
-  res.end(`Item: ${slug}`)
-})
+// app.get('/api/item/:slug', (req, res) => {
+//   const { slug } = req.params
+//   res.end(`Item: ${slug}`)
+// })
 
 app.listen(port, () => {
   console.log('Eyyy tu app corre por el puerto ' + port)
