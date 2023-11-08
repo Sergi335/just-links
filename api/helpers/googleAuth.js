9057e9e61975e573ff2a95022ec4db4347623568
Sergio <sergiadn335@gmail.com>
2023-11-08 02:18:32 +0100

    Security update

--- api/helpers/googleAuth.js
+++ api/helpers/googleAuth.js
@@ -1,6 +1,6 @@
 const { getAuth } = require('firebase-admin/auth')
 const admin = require('firebase-admin')
-const serviceAccount = require('../config/justlinks-7330b-firebase-adminsdk-lxi21-31ef679de3.json')
+const serviceAccount = require('../../firebase-secret/justlinks-7330b-firebase-adminsdk-lxi21-31ef679de3.json')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
